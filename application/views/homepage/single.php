b61070ed9433383d1380e67e054729ca96d3f3e6
fuadajip <fuadajipratomo@gmail.com>
2018-01-15 19:55:18 +0700

    optimizing layout

--- application/views/homepage/single.php
+++ application/views/homepage/single.php
@@ -1,18 +1,19 @@
 <!-- Page Content -->
-<div class="container">
+<div class="container" style="padding: 3em 0;">
 	<?php
 			foreach ($sql as $obj){
 		?>
-		<!-- Page Heading/Breadcrumbs -->
-		<h1 class="mt-4 mb-3">
-			<?php echo $obj->post_title; ?>
-		</h1>
+        <!-- Page Heading/Breadcrumbs -->
+
 
 		<div class="row">
 
 			<!-- Post Content Column -->
 			<div class="col-lg-8 offset-md-2">
-
+                        
+                <h1 class="mt-4 mb-3" >
+                    <?php echo $obj->post_title; ?>
+                </h1>
 				<!-- Preview Image -->
 				<img class="img-fluid rounded" src="<?php echo base_url();?>./uploads/<?php echo $obj->post_img;?> " alt="">
 
@@ -24,7 +25,7 @@
 				<hr>
 
 				<!-- Post Content -->
-                <div class="post-detail">
+                <div class="post-detail" style="padding: 3em 0;">
                 <?php echo $obj->post_desc;?>
                 </div>
 
